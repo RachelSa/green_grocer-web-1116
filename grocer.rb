61b3a9f06c84a7e7217b0fb628bf34a97733a403
Rachel Salois <rachel.salois@gmail.com>
2016-11-13 22:49:26 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -23,12 +23,40 @@ result
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  if coupons.length == 0
+	return cart
 end
+  	result = {}
+  	adjusted = {}
+  coupons.each do |c|
+  	cart.each do |food, detail|
+  			if food == c[:item]
+  				num_coups= cart[food][:count] / c[:num]
+  				coups = c[:cost] * (num_coups)
+  				clear = cart[food][:clearance]
+  				adjusted["#{food} W/COUPON"] = {price: coups, clearance: clear, count: num_coups}
+  				remaining = cart[food][:count] % c[:num]
+  				result[food] = detail
+  				result[food][:count] = remaining
+  			else result[food] = detail
+  			end
+  		end
+  	end
+  	result.merge!(adjusted)
+  	cart = result
+  	cart
+  end
 
-def apply_clearance(cart)
-  # code here
-end
+
+  def apply_clearance(cart)
+    cart.each do |item, details|
+  	if cart[item][:clearance] == true
+  		cart[item][:price] = (0.8 * cart[item][:price]).round(2)
+  	end
+  	end
+    cart
+  end
+
 
 def checkout(cart, coupons)
   # code here
